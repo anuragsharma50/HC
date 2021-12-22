a8e695ec3b1ba6573746a7d3e9e134bf0a9e1298
Anurag Sharma <anuragsh868@gmail.com>
2021-12-22 23:16:17 +0530

    cookie banner and policy added

--- pages/cookie-policy.js
+++ pages/cookie-policy.js
@@ -16,7 +16,7 @@ function CookiePolicy() {
                 <h1>Cookie Policy for Happie Celebrations</h1>
 
                 <h2>What Are Cookies</h2>
-                <p>As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or 'break' certain elements of the sites functionality.</p>
+                <p>As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or &apos;break&apos; certain elements of the sites functionality.</p>
   
                 <h2>How We Use Cookies</h2>
                 <p>We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.</p>
@@ -58,7 +58,7 @@ function CookiePolicy() {
                 </ul>
 
                 <h2>More Information</h2>
-                <p>Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren't sure whether you need or not it's usually safer to leave cookies enabled in case it does interact with one of the features you use on our site.</p>
+                <p>Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren&apos;t sure whether you need or not it&apos;s usually safer to leave cookies enabled in case it does interact with one of the features you use on our site.</p>
                 <p>However if you are still looking for more information then you can contact us through one of our preferred contact methods:</p>
 
                 <ul>
